8ce31e1ef0609da34cc8302c1410afd9fecd7616
Laraib <laraib15869@gmail.com>
2026-03-15 20:11:42 +0500

    Update routes.py

---
+++ service/routes.py
@@ -0,0 +1,20 @@
+def read_product():
+    pass
+
+def update_product():
+    pass
+
+def delete_product():
+    pass
+
+def list_products():
+    pass
+
+def list_by_name():
+    pass
+
+def list_by_category():
+    pass
+
+def list_by_availability():
+    pass

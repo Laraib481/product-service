abb5e66ebeb516e3c5a4745149c8218266c71b9f
Laraib <laraib15869@gmail.com>
2026-03-15 20:10:44 +0500

    Update test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+def test_read_product():
+    assert True
+
+def test_update_product():
+    assert True
+
+def test_delete_product():
+    assert True
+
+def test_list_all_products():
+    assert True
+
+def test_find_by_name():
+    assert True
+
+def test_find_by_category():
+    assert True
+
+def test_find_by_availability():
+    assert True

7c52d79d62d13f66ef2875696e375abf7b67c0ea
Laraib <laraib15869@gmail.com>
2026-03-15 20:11:18 +0500

    Update test_routes.py

---
+++ tests/test_routes.py
@@ -0,0 +1,20 @@
+def test_get_product():
+    assert True
+
+def test_update_product():
+    assert True
+
+def test_delete_product():
+    assert True
+
+def test_list_all_products():
+    assert True
+
+def test_list_by_name():
+    assert True
+
+def test_list_by_category():
+    assert True
+
+def test_list_by_availability():
+    assert True
